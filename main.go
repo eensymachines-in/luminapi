fb32da01f6a6e1e0f4dc6badcf68561f668880f6
Kneerunjun <kneerunjun@gmail.com>
2021-04-13 21:14:21 +0530

    alterations to docker-compose and log settings

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"os"
 	"strings"
 	"time"
 
+	utl "github.com/eensymachines-in/utilities"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 )
@@ -18,8 +20,20 @@ const (
 	fMqttsecret = "/run/secrets/mqtt_secret"
 )
 
+var (
+	// Flog : determines if the direction of log output
+	Flog bool
+	// FVerbose :  determines the level of log
+	FVerbose bool
+)
+
 // Setting the environment variables here and prepare before the api server runs
 func init() {
+	/*Reading in the command line params, at the entrypoint*/
+	utl.SetUpLog()
+	flag.BoolVar(&Flog, "flog", true, "direction of log messages, set false for terminal logging. Default is true")
+	flag.BoolVar(&FVerbose, "verbose", false, "Determines what level of log messages are to be output, Default is info level")
+
 	file, err := os.Open(fMqttsecret)
 	if err != nil {
 		log.Warnf("Failed to read encryption secrets, please load those %s", err)
@@ -48,7 +62,13 @@ func init() {
 	}
 }
 func main() {
-
+	/*This will setup the logging direction and depth*/
+	flag.Parse()
+	logFile := os.Getenv("LOGF")
+	if logFile != "" {
+		closeLogFile := utl.CustomLog(Flog, FVerbose, logFile) // Log direction and the level of logging
+		defer closeLogFile()
+	}
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 	r.Use(CORS)
