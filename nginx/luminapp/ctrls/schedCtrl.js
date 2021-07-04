267a7e82639bc87d62eee416b15771b093864c68
Kneerunjun <kneerunjun@gmail.com>
2021-07-04 07:39:16 +0530

    #17, new submit logic

--- nginx/luminapp/ctrls/schedCtrl.js
+++ nginx/luminapp/ctrls/schedCtrl.js
@@ -3,7 +3,7 @@
     uuid of the device is from the $routeParams.serial
     A deep watch on the schedules will help to extend the object for the validation functions
     For the first change it also records a comparison JSON string. This when compare to the later state of schedules will let us know if anything has changed*/ 
-    angular.module("luminapp").controller("schedCtrl", function($scope, $routeParams,srvApi, $route,srvRefactor, $rootScope,$timeout){
+    angular.module("luminapp").controller("schedCtrl", function($scope, $routeParams,srvApi,srvRefactor,$route,$rootScope){
         $scope.wait = false; //used to show/hide the ribbon progress bar
         $scope.optsSchedules = [];
         $scope.selectedSched = null; // this schedule is the pointer to selected one
@@ -39,7 +39,7 @@
             result = angular.extend({}, s)
             result.name = s.primary?"primary":"overlay-"+$scope.optsSchedules.length;
             result.title =s.primary?"Primary schedule":"Overlay schedule";
-            result.desc = s.primary?"Is a wide policy, applied onto all the nodes. Apply individual node exceptions ahead of this. Cannot delete but only modify the primary schedule.":"This policy is applied atop the primary schedule. Its an exception for the specific nodes. Can be deleted and modified.";
+            result.desc = s.primary?"Is a wide policy, applied onto all the nodes. Apply individual node exceptions ahead of this. Cannot delete but only modify the primary schedule.":"Overlay schedules are exceptions atop primary. They can be modified/deleted. Applies to select few nodes / all nodes";
             result.oid= uuoid(); //so that we can track the object quickly when modifying the list
             result.lbls = function(){
                 // getting rmaps definitions from ids that the schedule signifies 
@@ -61,7 +61,14 @@
                     r.push(item)
                 });
                 return r
-            }()
+            }();
+            result.has_zero_nodes = function(){
+                // this will calculate if the schedule option has no node selected 
+                // filters all the labels that have been selected 
+                // if length of labels selected ==0 then schedule has zero node selected
+                // this will help in identifying this schedule before submit
+                return result.lbls.filter(x=>x.sel==true).length ==0 ? true: false
+            }
             return result
         }
         // User by clicking on the New button will trigger this
@@ -100,31 +107,47 @@
         }, "deviceDetails") //getting the schedules would get all the device details
         // schedules need the relay maps as well 
         $scope.submit = function(){
-            scheds = [] ; // array of schedules ready to be dispatched to the api
-            // below we go ahead to transform the optsSchedules model to sched model as required by the api
-            $scope.optsSchedules.forEach(function(el){
-                scheds.push({
-                    on:el.on, 
-                    off:el.off, 
-                    primary:el.primary,
-                    ids: function(){
-                        re = [];
-                        el.lbls.forEach(x=>{
-                            if (x.sel == true){
-                                re.push(x.rid);
-                            }
-                        });
-                        return re
-                    }()
+            if ($scope.optsSchedules.filter(x=>x.has_zero_nodes()==true).length==0){
+                // Normal case when the user has selected atleast one node on each schedule
+                scheds = []; // array of schedules ready to be dispatched to the api
+                // below we go ahead to transform the optsSchedules model to sched model as required by the api
+                $scope.optsSchedules.forEach(function(el){
+                    scheds.push({
+                        on:el.on, 
+                        off:el.off, 
+                        primary:el.primary,
+                        ids: function(){
+                            // this will determine if the user has selected atleast one node
+                            // if none are selected we need to mandate selection of atleast one
+                            re = [];
+                            el.lbls.filter(x=>x.sel ==true).forEach(x=>re.push(x.rid))
+                            return re
+                        }()
+                    })
+                });
+                // this goes into #16
+                payload = {
+                    serial: $scope.deviceDetails.serial,
+                    scheds: scheds
+                }
+                srvApi.patch_device_schedules(payload.serial, payload).then(function(data){
+                    console.log("Success! schedules have been saved");
+                    console.log(data);
+                    $route.reload();
+                },function(error){
+                    error.upon_exit = function(){
+                        // this runs when the modal is dismissed 
+                        $scope.$apply(function(){
+                            $route.reload()
+                        })
+                    }
+                    $rootScope.err = error;
                 })
-            })
-            // this goes into #16
-            payload = {
-                serial: $scope.deviceDetails.serial,
-                scheds: scheds
+            }else {
+                console.error("One or more schedules have no nodes selected");
+                console.error("All schedules need to have aleast one node selected")
             }
-            console.log("Change payload now ready to be sent to server");
-            console.log(payload)
+                
         }
     })
 })()
